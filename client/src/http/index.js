55e2e835971c975039e3cab17d1b6ea46397db15
Vademzip <vadim.sardarov@yandex.ru>
2023-05-01 22:26:11 +0300

    Установлен Axios

---
+++ client/src/http/index.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+const $host =  axios.create({
+    baseURL: import.meta.env.VITE__API_URL
+})
+
+
+const $authHost =  axios.create({
+    baseURL: import.meta.env.VITE__API_URL
+})
+
+const authInterceptor = config => {
+    config.headers.authorization = `Bearer ${localStorage.getItem('token')}`
+    return config
+}
+
+$authHost.interceptors.request.use(authInterceptor)
+
+export {
+    $host,
+    $authHost
+}
